cdb0c893705c44ffcca8135f6d4c4fcda77bd47a
sharm <sharm@Lovees-pc>
2023-09-16 18:09:52 +0530

    commit new changes - 16sep

---
+++ Introduction/src/AutomationPractise.java
@@ -0,0 +1,11 @@
+
+public class AutomationPractise {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hi");
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Introduction/src/Practise1.java
+++ Introduction/src/Practise1.java
@@ -4,6 +4,8 @@ public class Practise1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GIT HUB 1st Coommit");
+		System.out.println("GIT HUB 2ND Coommit");
+		
 
 	}
 
